bfba57efcd340191ffddaeb08d0c5a09d6a32845
Sergio Campamá <kaipi@copilot.money>
2024-02-24 18:52:01 -0300

    Initial Commit

---
+++ SessionsTestApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SessionsTestApp
+//
+//  Created by Sergio Campama on 24/2/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
