733abf4389bbd2f10f06afb5f8f02813a5480aa4
Sergio Campamá <kaipi@copilot.money>
2024-02-24 19:07:59 -0300

    Initial commit

---
+++ Podfile
@@ -0,0 +1,17 @@
+platform :ios, '15'
+use_frameworks! :linkage => :static
+
+inhibit_all_warnings!
+
+target 'SessionsTestApp' do
+  use_modular_headers!
+  pod 'FirebaseAnalytics'
+  pod 'FirebaseAuth'
+  pod 'FirebaseCrashlytics'
+  pod 'FirebaseFirestore'
+  pod 'FirebaseFunctions'
+  pod 'FirebaseMessaging'
+  pod 'FirebaseStorage'
+  pod 'FirebaseRemoteConfig'
+  pod 'FirebaseDynamicLinks'
+end
--- SessionsTestApp/AppDelegate.swift
+++ SessionsTestApp/AppDelegate.swift
@@ -4,16 +4,58 @@
 //
 //  Created by Sergio Campama on 24/2/24.
 //
-
+import FirebaseCore
 import UIKit
+
+struct FirebaseOptionsPlist {
+  let clientID: String
+  let reversedClientID: String
+  let apiKey: String
+  let gcmSenderID: String
+  let bundleID: String
+  let projectID: String
+  let storageBucket: String
+  let isAdsEnabled: Bool
+  let isAnalyticsEnabled: Bool
+  let isAppInviteEnabled: Bool
+  let isGCMEnabled: Bool
+  let isSignInEnabled: Bool
+  let googleAppID: String
+}
+
+private let productionConfig = FirebaseOptionsPlist(
+  clientID: "445606440735-nmgdjiedjntfcdf7gt3iie43v1r0sp5t.apps.googleusercontent.com",
+  reversedClientID: "com.googleusercontent.apps.445606440735-nmgdjiedjntfcdf7gt3iie43v1r0sp5t",
+  apiKey: "AIzaSyDuO9g8DnojBpuHdLdZW37iCbC1Ng_udGs",
+  gcmSenderID: "692425658344",
+  bundleID: "dev.kaipi.SessionsTestApp",
+  projectID: "sessionserror",
+  storageBucket: "sessionserror.appspot.com",
+  isAdsEnabled: false,
+  isAnalyticsEnabled: false,
+  isAppInviteEnabled: true,
+  isGCMEnabled: true,
+  isSignInEnabled: true,
+  googleAppID: "1:692425658344:ios:7e2138099e0f7a3b185e5d"
+)
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
-
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-    // Override point for customization after application launch.
+
+    let options = FirebaseOptions(
+      googleAppID: productionConfig.googleAppID,
+      gcmSenderID: productionConfig.gcmSenderID
+    )
+
+    options.apiKey = productionConfig.apiKey
+    options.clientID = productionConfig.clientID
+    options.projectID = productionConfig.projectID
+    options.storageBucket = productionConfig.storageBucket
+    options.bundleID = productionConfig.bundleID
+
+    FirebaseApp.configure(options: options)
+
     return true
   }
 
